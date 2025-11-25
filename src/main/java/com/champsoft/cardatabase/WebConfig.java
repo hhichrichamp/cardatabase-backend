1d484417c2f5e564d5771c33c3a154784a5aa2d4
hhichriChamp <haikel.amazon@gmail.com>
2025-11-25 11:26:44 -0500

    added frontend URL to config.java

--- src/main/java/com/champsoft/cardatabase/WebConfig.java
+++ src/main/java/com/champsoft/cardatabase/WebConfig.java
@@ -13,7 +13,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000") // your React dev URL(s)
+                        .allowedOrigins("http://localhost:5173",
+                                "https://champcars.onrender.com/",
+                                "http://localhost:3000") // your React dev URL(s)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
